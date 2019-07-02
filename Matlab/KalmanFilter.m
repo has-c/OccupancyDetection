00bcc451036b092b22903866a2408339bb2bc3f1
Hasnain Cheena <hasnaincheena.hc@gmail.com>
2019-07-02 13:27:17 +1200

    sampling time fix kalman filter

--- Matlab/KalmanFilter.m
+++ Matlab/KalmanFilter.m
@@ -2,8 +2,8 @@
 %Kalman Filter Design Preliminary
 %Author: Hasnain Cheena 
 
-deltaT = 0.02; %set sampling time
-
+deltaT = 0.001;
+Ts = deltaT;
 %state space model
 A = [0 1 0 0;
     2/(deltaT)^2 -2/deltaT 0 0;
@@ -15,20 +15,20 @@ B = [0 0;
     0 0;
     0 -2/(deltaT)^2];
 
-C = [1 0 1 0];
+C = [1 1 1 0];
 
 D = 0;
-
 %plant model
-plant = ss(A,B,C,D);
+plant = ss(A,B,C,D,Ts);
 
 %observability
 observabilityMatrix = obsv(A,C);
 observabilityCheck = det(observabilityMatrix);
-
+    
 %process and measurement covariances
 W = 1;
 V = 1;
 
 %discrete kalman filter
 [kalmf, L, P, M] = kalman(plant, W,V);
+outputEstimate = kalmf(1,:);
