aa26f46425f1718c24ddb22c7c058ab2e96de596
Hasnain Cheena <hasnaincheena.hc@gmail.com>
2019-08-08 15:46:08 +1200

    k medoids

--- Python/KMedoids.py
+++ Python/KMedoids.py
@@ -1,43 +1,64 @@
 import numpy as np
 import random
 
-def cluster(distances, k):
+def kMedoids(D, k, tmax=100):
+    # determine dimensions of distance matrix D
+    m, n = D.shape
 
-    m = distances.shape[0] # number of points
+    if k > n:
+        raise Exception('too many medoids')
 
-    # Pick k random medoids.
-    curr_medoids = np.array([-1]*k)
-    while not len(np.unique(curr_medoids)) == k:
-        curr_medoids = np.array([random.randint(0, m - 1) for m in range(k)])
-    old_medoids = np.array([-1]*k) # Doesn't matter what we initialize these to.
-    new_medoids = np.array([-1]*k)
-   
-    # Until the medoids stop updating, do the following:
-    while not ((old_medoids == curr_medoids).all()):
-        # Assign each point to cluster with closest medoid.
-        clusters = assign_points_to_clusters(curr_medoids, distances)
+    # find a set of valid initial cluster medoid indices since we
+    # can't seed different clusters with two points at the same location
+    valid_medoid_inds = set(range(n))
+    invalid_medoid_inds = set([])
+    rs,cs = np.where(D==0)
+    # the rows, cols must be shuffled because we will keep the first duplicate below
+    index_shuf = list(range(len(rs)))
+    np.random.shuffle(index_shuf)
+    rs = rs[index_shuf]
+    cs = cs[index_shuf]
+    for r,c in zip(rs,cs):
+        # if there are two points with a distance of 0...
+        # keep the first one for cluster init
+        if r < c and r not in invalid_medoid_inds:
+            invalid_medoid_inds.add(c)
+    valid_medoid_inds = list(valid_medoid_inds - invalid_medoid_inds)
 
-        # Update cluster medoids to be lowest cost point. 
-        for curr_medoid in curr_medoids:
-            cluster = np.where(clusters == curr_medoid)[0]
-            new_medoids[curr_medoids == curr_medoid] = compute_new_medoid(cluster, distances)
+    if k > len(valid_medoid_inds):
+        raise Exception('too many medoids (after removing {} duplicate points)'.format(
+            len(invalid_medoid_inds)))
 
-        old_medoids[:] = curr_medoids[:]
-        curr_medoids[:] = new_medoids[:]
-        
-        print('UPDATE')
+    # randomly initialize an array of k medoid indices
+    M = np.array(valid_medoid_inds)
+    np.random.shuffle(M)
+    M = np.sort(M[:k])
 
-    return curr_medoids
+    # create a copy of the array of medoid indices
+    Mnew = np.copy(M)
 
-def assign_points_to_clusters(medoids, distances):
-    distances_to_medoids = distances[:,medoids]
-    clusters = medoids[np.argmin(distances_to_medoids, axis=1)]
-    clusters[medoids] = medoids
-    return clusters
+    # initialize a dictionary to represent clusters
+    C = {}
+    for t in range(tmax):
+        # determine clusters, i. e. arrays of data indices
+        J = np.argmin(D[:,M], axis=1)
+        for kappa in range(k):
+            C[kappa] = np.where(J==kappa)[0]
+        # update cluster medoids
+        for kappa in range(k):
+            J = np.mean(D[np.ix_(C[kappa],C[kappa])],axis=1)
+            j = np.argmin(J)
+            Mnew[kappa] = C[kappa][j]
+        np.sort(Mnew)
+        # check for convergence
+        if np.array_equal(M, Mnew):
+            break
+        M = np.copy(Mnew)
+    else:
+        # final update of cluster memberships
+        J = np.argmin(D[:,M], axis=1)
+        for kappa in range(k):
+            C[kappa] = np.where(J==kappa)[0]
 
-def compute_new_medoid(cluster, distances):
-    mask = np.ones(distances.shape)
-    mask[np.ix_(cluster,cluster)] = 0.
-    cluster_distances = np.ma.masked_array(data=distances, mask=mask, fill_value=10e9)
-    costs = cluster_distances.sum(axis=1)
-    return costs.argmin(axis=0, fill_value=10e9)
+    # return results
+    return M, C
